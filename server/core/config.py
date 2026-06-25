a2a783ead3b590dba7b4b9950750e013020fc813
Trivo121 <viswa225574@gmail.com>
2026-06-25 13:24:13 +0530

    let's see

--- server/core/config.py
+++ server/core/config.py
@@ -12,6 +12,6 @@ class Settings(BaseSettings):
     RAZORPAY_KEY_ID: str = ""
     RAZORPAY_KEY_SECRET: str = ""
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 settings = Settings()
